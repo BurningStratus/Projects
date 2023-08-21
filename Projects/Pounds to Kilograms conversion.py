72a0b8fde10e0e4be97392216dd84ed40a5bcaa0
BurningStratus <pavel.shishkin@protonmail.com>
2023-08-21 20:47:42 +0300

    Made convertor, added useless shit

--- Projects/Pounds to Kilograms conversion.py
+++ Projects/Pounds to Kilograms conversion.py
@@ -2,3 +2,4 @@ weight_str = input("Enter the weight in Pounds: ")
 weight = float(weight_str)
 kilograms = weight * 0.453592
 print(weight_str + " is about " + str(kilograms) + " kg.")
+print(str(weight + kilograms))
