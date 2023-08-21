83a99dbe60786513a340fb38bb5e4bf6f624f20c
BurningStratus <pavel.shishkin@protonmail.com>
2023-08-21 21:17:10 +0300

    We fix that.

---
+++ Projects/Harjoittelu2.py
@@ -0,0 +1,5 @@
+import math
+radius = input("Insert radius of a circle: ")
+radius_str = float(radius)
+area = (math.pi*(radius_str**2))
+print("Area of a circle with a radius of " + radius + " is: " + str(area))
