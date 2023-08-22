c4b68998cb2ec4bfb13db7ef9aca8df7d29f6020
BurningStratus <pavel.shishkin@protonmail.com>
2023-08-22 10:21:38 +0300

    Random codes gen.

---
+++ Projects/Block 2/Harjoitus6.py
@@ -0,0 +1,21 @@
+# Program should give 2 random codes:
+# 3-digit codes which each number vry 0-9
+# 4-digit codes which each number vry 1-6
+
+#random.int(a, b, c)
+
+import random
+
+digit1 = random.randint(0, 9)
+digit2 = random.randint(0, 9)
+digit3 = random.randint(0, 9)
+
+print("This program gives two random codes.")
+print("3-Digit random code: ", digit1, digit2, digit3)
+
+dig1 = random.randint(1, 6)
+dig2 = random.randint(1, 6)
+dig3 = random.randint(1, 6)
+dig4 = random.randint(1, 6)
+
+print("4-Digit random code: ", dig1, dig2, dig3, dig4)
