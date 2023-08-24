e88320ce00516d1d703fb1be866148c4aba68394
BurningStratus <pavel.shishkin@protonmail.com>
2023-08-24 10:43:44 +0300

    Leap year counter( counts only from some dude's resurrection), tweaks

--- Projects/Block 3/Harjoitus4.py
+++ Projects/Block 3/Harjoitus4.py
@@ -1,8 +1,8 @@
 year = float(input("Enter the year: "))
 
 if year % 100 == 0 and year % 400 == 0:
-    print(f"{year:5.0f}", "Is a leap year.", sep=" ")
+    print(f"{year:5.0f}", "Is a leap year.", sep="")
 elif year % 4 == 0:
-    print(f"{year:5.0f}", "Is a leap year.", sep=" ")
+    print(f"{year:5.0f}", "Is a leap year.", sep="")
 else:
-    print(f"{year:5.0f}", "Is not a leap year.", sep="  ")
+    print(f"{year:5.0f}", "Is not a leap year.", sep=" ")
