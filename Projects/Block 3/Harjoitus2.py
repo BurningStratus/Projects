8b3d3380b9379ab531aa0c03aef575214e0f6d8a
BurningStratus <pavel.shishkin@protonmail.com>
2023-08-28 14:38:54 +0300

    Changed lil birt

--- Projects/Block 3/Harjoitus2.py
+++ Projects/Block 3/Harjoitus2.py
@@ -3,10 +3,10 @@
 #- B on ikkunaton hytti autokannen yläpuolella.
 #- C on ikkunaton hytti autokannen alapuolella.
 
-print("Get description of your floor type here.\nAvailable floor types are: LUX, A, B, C.")
+print("Get description of your floor type.\nAvailable floor types are: LUX, A, B, C.\n ")
 floor = input("Enter you floor type: ")
 
-if floor in {"LUX", "Lux"}:
+if floor in {"LUX", "Lux", "lux"}:
     print("LUX is the best available class of floors.\nBalcony on the highest floor with the beautiful view!")
 
 elif floor == "A":
@@ -15,6 +15,6 @@ elif floor == "A":
 elif floor == "B":
     print("B class is for those who want the balance between the price and comfort.\nWindows aren't an option!")
 elif floor == "C":
-    print("C is the cheapest option to only to get to the destination.\nNo windows, you live with the cars.")
+    print("C is the cheapest option to only get to the destination.\nNo windows, you live with the cars.")
 else:
     print("Prompt isn't recognised. Try again.")
