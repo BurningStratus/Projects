622832ce3342725851925277af3ebbc46f71e38a
BurningStratus <pavel.shishkin@protonmail.com>
2023-08-28 11:41:43 +0300

    Ohjelma antaa kaikki numerot 1-1000, jotka ovat jaollisia 3.

---
+++ Projects/Block 4/Harjoitus 1.py
@@ -0,0 +1,10 @@
+print("This program checks if the number is dividable by 3.\n######################################################")
+
+num = 0
+
+while num < 1000:
+    num = num + 1
+    if num % 3 == 0:
+        print(num)
+
+print("All numbers listed.")
