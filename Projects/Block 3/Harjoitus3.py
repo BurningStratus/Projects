97849d25ed33818e2e805b695b16eaf8a8aec66b
BurningStratus <pavel.shishkin@protonmail.com>
2023-08-29 09:58:03 +0300

    Added while

--- Projects/Block 3/Harjoitus3.py
+++ Projects/Block 3/Harjoitus3.py
@@ -1,20 +1,60 @@
 # Naisen normaali hemoglobiiniarvo on välillä 117-175 g/l.
 # Miehen normaali hemoglobiiniarvo on välillä 134-195 g/l.
+comma = input("start/stop> ")
 
-mf = input("Gender(Male/Female): ")
-gmgl = float(input("Hemoglobin level: "))
+mf = input("Gender(Male/Female)> ")
+gmgl = float(input("Hemoglobin level> "))
 
-if mf == "Male":
-    if gmgl > 195:
-        print("Level too high!")
-    elif 195 >= gmgl >= 134:
-        print("Good! Level is normal.")
-    elif gmgl < 134:
-        print("Level too low!")
-elif mf == "Female":
-    if gmgl > 175:
-        print("Level too high!")
-    elif 174 >= gmgl >= 117:
-        print("Good! Level is normal.")
-    elif gmgl < 117:
-        print("Level too low!")
+while comma not in {"stop"} or mf not in {"stop"}:
+    if mf == "Male" or mf == "male":
+        if gmgl > 195:
+            print("Level too high!")
+
+            mf = input("Gender(Male/Female): ")
+            gmgl = float(input("Hemoglobin level: "))
+
+            comma = input("start/stop> ")
+
+        elif 195 >= gmgl >= 134:
+            print("Good! Level is normal.")
+            mf = input("Gender(Male/Female): ")
+            gmgl = float(input("Hemoglobin level: "))
+
+            comma = input("start/stop> ")
+
+        elif gmgl < 134:
+            print("Level too low!")
+            mf = input("Gender(Male/Female): ")
+            gmgl = float(input("Hemoglobin level: "))
+
+            comma = input("start/stop> ")
+
+    elif mf == "Female" or mf == "female":
+
+        if gmgl > 175:
+            print("Level too high!")
+            mf = input("Gender(Male/Female): ")
+            gmgl = float(input("Hemoglobin level: "))
+
+            comma = input("start/stop> ")
+
+        elif 174 >= gmgl >= 117:
+            print("Good! Level is normal.")
+            mf = input("Gender(Male/Female): ")
+            gmgl = float(input("Hemoglobin level: "))
+
+            comma = input("start/stop> ")
+
+        elif gmgl < 117:
+            print("Level too low!")
+            mf = input("Gender(Male/Female): ")
+            gmgl = float(input("Hemoglobin level: "))
+
+            comma = input("start/stop> ")
+    else:
+        print("Invalid prompt!")
+        mf = input("Gender(Male/Female): ")
+        gmgl = float(input("Hemoglobin level: "))
+
+else:
+    comma = input("start/stop> ")
