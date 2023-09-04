3091f3367bb9e29a61237c702421da8d1ef7c708
BurningStratus <pavel.shishkin@protonmail.com>
2023-09-04 18:28:09 +0300

    Gamble w/ 3 tries

--- Projects/Block 4/Harjoitus 4.py
+++ Projects/Block 4/Harjoitus 4.py
@@ -4,29 +4,30 @@ print("\nGuess the number. Think twice: you only have 3 tries!\n")
 
 gmb = random.randint(1, 10)
 
-print(gmb)
-
+tries = 3
+print(f"Tries left: {tries}")
 gss = int(input("Your guess> "))
 
-tries = 2
+
+tries = tries - 1
 
 while tries > 0 and gss != gmb:
 
     if gss < gmb:
-        print(gmb)
+#        #print(gmb)
         print("Not enough!")
-        print(tries)
+        print(f"Tries left: {tries}")
         tries = tries - 1
 
     elif gss > gmb:
-        print(gmb)
+#        print(gmb)
         print("Too much!")
-        print(tries)
+        print(f"Your tries: {tries}")
         tries = tries - 1
 
     gss = int(input("Your guess> "))
 
 if gss == gmb :
-    print('you win')
+    print('You win!')
 else:
-    print('you f***ing looser')
+    print('You lose!')
