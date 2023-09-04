d4ac2593c1fe8a563a565d9db47308d195c70c92
BurningStratus <pavel.shishkin@protonmail.com>
2023-09-04 18:27:31 +0300

    Math be hittin hard

--- Projects/Block 4/Harjoitus 6.py
+++ Projects/Block 4/Harjoitus 6.py
@@ -1,18 +1,20 @@
 import random
 
 nnumber = int(input("Amount of random points: "))
-
+print("working...")
 rev = 0
 hit = 0
 
 while rev <= nnumber:
+
     x = random.uniform(-1, 1)
     y = random.uniform(-1, 1)
-    while x ** 2 + y ** 2 < 1:
-        hit = hit + 1
-        break
-    rev = rev + 1
+
+    if x ** 2 + y ** 2 < 1:
+        hit += 1
+    rev += 1
 
 pi = 4 * hit / nnumber
 
-print(pi)
+print(f"The pi is: {pi}")
+
