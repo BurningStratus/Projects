e00d1519b226c5a375ea5455b9b0497e61577278
User1337 <0000@>
2023-09-05 17:35:28 +0300

    dead

---
+++ Freestyle/Hanging around1.py
@@ -0,0 +1,35 @@
+
+
+#list = ["cat","dog","pirate","jeff bozo","elona maska","alesha shevtsova"]
+#for p in list:
+#    print(q, len(q))
+
+# users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
+#
+# # Strategy:  Iterate over a copy
+# for user, status in users.copy().items():
+#     if status == 'inactive':
+#         del users[user]
+#
+# # Strategy:  Create a new collection
+# active_users = {}
+# for user, status in users.items():
+#     if status == 'active':
+# #         # active_users[user] = status
+#
+# a = ['я','блять','карлик']
+# for i in range(len(a)):
+#     print(i, a[i])
+
+# a = [1, 3, 4, 5, 89, 2, 12, 333, 2, 9]
+# cmd = input("Choose sort type(sorted/a.sort): ")
+#
+# if cmd in ("sorted","sotred"):
+#     sorted(a)
+#     print(a)
+# elif cmd in ("a.sort","a,srot","a.srot"):
+#     a.sort()
+#     print(a)
+#
+# sorted("This is a test string from Andrew".split(), key=str.lower)
+
