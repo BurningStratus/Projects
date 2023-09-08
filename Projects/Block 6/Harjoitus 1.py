2de136eff03f57a9396f966fe08b73288953d4e7
User1337 <0000@>
2023-09-08 15:13:51 +0300

    Dice thrower 1500

---
+++ Projects/Block 6/Harjoitus 1.py
@@ -0,0 +1,23 @@
+# Kirjoita parametriton funktio, joka palauttaa paluuarvonaan satunnaisen nopan silmäluvun väliltä 1..6.
+# Kirjoita pääohjelma, joka heittää noppaa niin kauan kunnes tulee kuutonen.
+# Pääohjelma tulostaa kunkin heiton jälkeen saadun silmäluvun.
+
+#  function w/ no parameter, which throws dice
+#  global program which throws dice until it gets 6
+#  global program shows all attempts and nums it got
+
+import random
+thr = 0
+num = 0
+def dicer():
+    print("Throwing dice...")
+    dice = random.randint(1,6)
+    print(f"I threw a dice and got {dice}")
+    return dice
+
+while num != 6:
+    num = dicer()
+    thr += 1
+    print(f"I threw a dice {thr} times already\n")
+else:
+    print(f"\nIt took me {thr} throws to get six.")
