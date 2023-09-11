ce2fec0e794b7a7ceec3970fa913257437d5abe1
User1337 <0000@>
2023-09-11 12:12:23 +0300

    Sandbox of 6.5

--- Freestyle/Teht 6.5 sandbox.py
+++ Freestyle/Teht 6.5 sandbox.py
@@ -1,13 +1,19 @@
-list = [1,2,3,4,5]
+list = [1,83,2,3,53,4,5,15]
 
 
 
 #for luku_jota_vastaan_testataan in range(3, luku_jota_testataan - 1):
 
-for c in list:
-    for i in range(3, c-1):
-        if c % i == 0:
-            list.remove(i)
-            print(i)
+# for c in list:
+#     for i in range(3, c-1):
+#         if c % i == 0:
+#             list.remove(i)
+#             print(i)
+#
+# print(list)
 
+for i in list:
+    for c in range(3, i-1):
+        if i % 2 != 0:
+            list.remove(i)
 print(list)
