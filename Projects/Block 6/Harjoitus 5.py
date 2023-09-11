cf5830b923a318b4646765e54ebf8435f41ee40c
User1337 <0000@>
2023-09-11 11:30:51 +0300

    many questions, no answers.

---
+++ Projects/Block 6/Harjoitus 5.py
@@ -0,0 +1,29 @@
+# function, which gets a list of integers
+# global prog which deletes all non-prime numbers.
+
+#for luku_jota_vastaan_testataan in range(3, luku_jota_testataan - 1):
+
+userlist = []
+
+
+def noprime(user_list):  # function to evaluate everything from a user-def. list
+    user_list.sort()
+    for i in user_list:
+        if i % 2 != 0:
+            user_list.remove(i)
+        elif i % 2 == 1:
+            user_list.remove(i)
+
+
+sum_list = input("Integer: ")
+# "While" for user to define a list
+while sum_list != "":
+    sum_list = int(sum_list)
+    userlist.append(sum_list)
+    print(userlist)
+    sum_list = input("Integer: ")
+
+# calling a function and calculating results.
+print(userlist)
+noprime(userlist)
+print(f"User-defined list without non-prime numbers:\n{userlist}.")
