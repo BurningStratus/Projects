0c03762df856ce3729a639489edc0ce422c79c91
BurningStratus <pavel.shishkin@protonmail.com>
2023-09-11 13:15:43 +0300

    it is always better to create a new list inside a function to add values there.

--- Projects/Block 6/Harjoitus 5.py
+++ Projects/Block 6/Harjoitus 5.py
@@ -7,14 +7,11 @@ userlist = []
 
 
 def noprime(user_list):  # function to evaluate everything from a user-def. list
-    #  user_list.sort()
+    nooprime = []
     for i in user_list:
-        if i % 2 != 0:
-            user_list.remove(i)
-
-    return user_list
-        # elif i % 2 == 1:
-        #     user_list.remove(i)
+        if i % 2 == 0:
+            nooprime.append(i)
+    return nooprime
 
 
 sum_list = input("Integer: ")
@@ -25,11 +22,7 @@ while sum_list != "":
     print(userlist)
     sum_list = input("Integer: ")
 
-# calling a function and calculating results.
 print(userlist)
-
-noprime(userlist)
-
-print(f"User-defined list without non-prime numbers:\n{userlist}.")
+print(f"User-defined list without odd numbers:\n{noprime(userlist)}.")
 
 #  https://realpython.com/python-return-statement/#returning-vs-printing
