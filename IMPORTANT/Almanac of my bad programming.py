ea2972ef1e6419a17ff4c8e94693280f1dc9bfd4
BurningStratus <pavel.shishkin@protonmail.com>
2023-09-12 09:56:01 +0300

    1. Modifying global variables

--- IMPORTANT/Almanac of my bad programming.py
+++ IMPORTANT/Almanac of my bad programming.py
@@ -1,12 +1,19 @@
 # Almanac of bad programming.
 
 # 1.[MODIFYING GLOBAL VARIABLES]
+
 # When creating a function that needs to make/modify a list, don't modify the initial one.
 # Create a new instead.
+# It IS better to create a new list inside a function to add needed values there.
 
 # This is an example of modifying a global variables, so the function is dependent on initial values.
 # This is considered a bad programming, because code is difficult to debug and maintain.
-# Also it IS better to create a new list inside a function to add needed values there.
+
+# My mistakes here are:
+# 1. I made a function which modified a global value
+# 2. I made a function that wasn't self-contained
+# 3. Function returned an implicit value(not a meaningful one)
+
 # Bad sport ahead    --->
 list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 def noprime(list_1):  # function to evaluate everything from a list.
