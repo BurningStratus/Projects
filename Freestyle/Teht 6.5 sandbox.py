647621f2c08ae8e7686ecaa93edba566679d4afe
BurningStratus <pavel.shishkin@protonmail.com>
2023-09-12 13:13:58 +0300

    Sandbox with lists and functions

--- Freestyle/Teht 6.5 sandbox.py
+++ Freestyle/Teht 6.5 sandbox.py
@@ -1,6 +1,6 @@
 list = [1,83,2,3,53,4,5,15]
-
-
+listed = []
+import random
 
 #for luku_jota_vastaan_testataan in range(3, luku_jota_testataan - 1):
 
@@ -11,9 +11,93 @@ list = [1,83,2,3,53,4,5,15]
 #             print(i)
 #
 # print(list)
+#
+# for i in list:
+#     for c in range(3, i-1):
+#         if i % 2 != 0:
+#             list.remove(i)
+# print(list)
 
-for i in list:
-    for c in range(3, i-1):
-        if i % 2 != 0:
-            list.remove(i)
-print(list)
+# def return_47():
+#     return 47
+#
+# num = return_47()
+# num
+
+# def get_even(numbers):
+#     return [num for num in numbers if not num % 2]
+#
+# print(get_even([1, 2, 3, 4, 5, 6]))
+#
+# randomint = random.randint(10, 100)
+# def nomore_400(random_list):
+#     list_nomore_400 = []
+#     for i in random_list:
+#         if not i > 400 and not i < 200:
+#             list_nomore_400.append(i)
+#     list_nomore_400.sort(reverse=True)
+#     return list_nomore_400
+#
+# for i in range(1, randomint):
+#     int = random.randint(0, 450)
+#     listed.append(int)
+#
+# print(nomore_400(listed))
+
+# def add(a, b):
+#
+#     result = a + b
+#
+#     return result
+#
+#
+# print(add(2, 2))
+#
+# def omit_return_stmt():
+#     # Omit the return statement
+#     return
+#
+# print(omit_return_stmt())
+
+
+# def bare_return():
+#     # Use a bare return
+#     return
+#
+# print(bare_return())
+#
+#
+# def return_none_explicitly():
+#     # Return None explicitly
+#     return None
+#
+# print(return_none_explicitly())
+
+# Example of bad programming
+
+# counter = 0
+#
+#
+# def increment():
+#     global counter
+#     counter += 1
+#
+#
+# increment()
+# print(counter)
+
+# def my_abs(number):
+#     if number > 0:
+#         return number
+#     elif number < 0:
+#         return -number
+#
+#
+# print(my_abs(-15))
+#
+#
+# print(my_abs(15))
+#
+# print(my_abs(0))
+
+# https://realpython.com/python-return-statement/#returning-true-or-false
