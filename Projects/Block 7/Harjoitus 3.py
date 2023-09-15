f6dc8fbae1c61c13ff1e7e0d1097eaf5a594ff38
User1337 <0000@>
2023-09-15 21:48:28 +0300

    Best thing ive ever done. 15.09.23

---
+++ Projects/Block 7/Harjoitus 3.py
@@ -0,0 +1,65 @@
+# prog has 3 functions: help, add new, find existing and end: prog asks for airports names and their codes
+
+airports = {}
+
+
+def addtodict(group):
+    name = input("name of the airport: ")
+    code = input("code of the airport: ")
+    if name not in group and code not in group:
+        group[name] = code
+        print("record added")
+        return group
+    elif name in group or code in group:
+        print("name or code already exists")
+        print(group[name])
+    return None
+
+
+def show(group):
+    print("...retrieving records...")
+    for i in group:
+        print(f"{i} [{airports[i]}]")
+    print("...all records listed...")
+    return None
+
+
+def find(group):
+    strng = input("find by name or code: ")
+    for i in group:
+        if strng == group[i]:
+            print(f"{i} corresponds with [{strng}] ")
+        elif i == strng:
+            print(f"{strng} corresponds with [{group[i]}]")
+    return 0
+
+
+def help_cmd():
+    print("Available prompts are:\nhelp\nadd\nshow\nfind\nexit")
+    print("Usage: [command] + ENTER\n")
+    return None
+
+print("####@##############@#######%#########@############$###\n")
+print("Database of airports. Names and ICAO-codes are stored.")
+print("########//Type help to view list of commands.//########\n")
+print("###########$#########@##########$######%###############")
+
+cmd = input("command: ")
+while cmd != "exit":
+    if cmd == "help":
+        help_cmd()
+        cmd = input("command: ")
+    elif cmd == "add":
+        addtodict(airports)
+        cmd = input("command: ")
+    elif cmd == "show":
+        show(airports)
+        cmd = input("command: ")
+    elif cmd == "find":
+        find(airports)
+        cmd = input("command: ")
+    else:
+        print("err: not supported prompt")
+        cmd = input("command: ")
+else:
+    print("Bye")
