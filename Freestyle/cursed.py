56b700fe42245b35c09a489d461e275c2fcf8ec5
BurningStratus <pavel.shishkin@protonmail.com>
2023-10-10 14:48:03 +0300

    Updated sum

--- Freestyle/cursed.py
+++ Freestyle/cursed.py
@@ -1,6 +1,43 @@
-def even_or_odd(num):
-    return "eovdedn"[num % 2:: 2]  # cursed
+# def even_or_odd(num):
+#     return "eovdedn"[num % 2:: 2]  # cursed
+#
+# print(even_or_odd(20))  # ?????
+#
+# print(even_or_odd(1))  # ??????
+#
 
-print(even_or_odd(20))  # ?????
+### prog which casts a dice.
+import random
 
-print(even_or_odd(1))  # ??????
+casted_dices = []
+
+
+def dice_caster(side, dice):
+    for i in range(dice):
+        casted = random.randint(1, side)
+        casted_dices.append(casted)
+    return casted_dices
+
+
+def avg_num(desired_list):
+
+    freq_list = {}
+
+    for outer in desired_list:
+        freq = 0
+        for inner in desired_list:
+            if inner == outer:
+                freq += 1
+            else:
+                pass
+        freq_list[outer] = freq
+
+    return freq_list
+
+
+side_thrower = int(input("How much sides does your dice have?: "))
+dice_thrower = int(input("How much dice you have?: "))
+
+print(dice_caster(side_thrower, dice_thrower))
+
+print(avg_num(casted_dices))
